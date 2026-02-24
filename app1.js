fd457563281f31faaf9807aceca26af6c6409aab
Raju Ahmed <rajur20m@gmail.com>
2026-02-24 20:03:44 +0600

    New Leson

---
+++ app1.js
@@ -0,0 +1,66 @@
+// get element
+
+//get element by class
+
+//const target=document.getElementsByClassName("d1");
+//console.log(target);// ata atka array provide kora
+
+
+//get element by id
+//const target1=document.getElementById("d1");
+//console.log(target1)// ata single value unique
+
+// get element by tag name
+
+//const target2=document.getElementsByTagName(h1);
+//console.log(taget2);//atao akta element of array provide kora
+
+
+// Dom loop if else
+
+// const allbox=document.getElementsByClassName("box");
+// for(let i=0;i<allbox.length;i++){
+//     const element=allbox[i];
+//     console.log(element);
+    
+//     if(element.innerText=="box5"){
+//         element.style.backgroundColor="red";
+//     }
+//     else{
+//         element.style.backgroundColor="green"
+//     }
+// }
+
+
+document.getElementById("handleadd").addEventListener("click",(event)=>{
+    const val=document.getElementById("searchbox").value ;
+
+    const comment=document.getElementById("comment-container");
+
+    const p=document.createElement("p");
+     p.classList.add("child");
+
+     p.innerText=val;
+        
+     comment.append(p);
+    
+       const allcomment=document.getElementsByClassName("child");
+        
+
+     document.getElementById("searchbox").value="";
+
+     for(const element of allcomment){
+        element.addEventListener("click",(e)=>{
+            e.target.parentNode.removeChild(element);
+        })
+     }
+
+     
+     
+
+
+})
+
+// const handlesearch=(event)=>{
+//     console.log("Hello Raju");
+// }
